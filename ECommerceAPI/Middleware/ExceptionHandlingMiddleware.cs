49119fc8ac3a242a59212499e66562578ad43ca0
PPSDEVJNKNNTHPADUL <jklpadul.itd.pps@gmail.com>
2023-03-03 17:49:53 +0800

    Fix Exception Handling Middleware

--- ECommerceAPI/Middleware/ExceptionHandlingMiddleware.cs
+++ ECommerceAPI/Middleware/ExceptionHandlingMiddleware.cs
@@ -51,7 +51,7 @@ namespace ECommerceAPI.Middleware
             {
                 BadRequestException => StatusCodes.Status400BadRequest,
                 NotFoundException => StatusCodes.Status404NotFound,
-                ValidationException => StatusCodes.Status422UnprocessableEntity,
+                FluentValidation.ValidationException => StatusCodes.Status422UnprocessableEntity,
                 _ => StatusCodes.Status500InternalServerError
             };
 
